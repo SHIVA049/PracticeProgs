be9bcd456f9d7c5638fccdfb7d151309a096fce2
Shivaling L N <som.neralagi@gmail.com>
2025-03-06 11:16:38 +0530

    Added Simple Print Program

---
+++ src/dev/lpa/domain/simplePrint.java
@@ -0,0 +1,12 @@
+package dev.lpa.domain;
+
+public class simplePrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Learning Git By Practice");
+
+	}
+
+}
